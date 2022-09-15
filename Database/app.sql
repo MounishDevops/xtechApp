1e5d4bf5a16256a9aad2b8564a51b4735410eba5
MounishDevops <94664987+MounishDevops@users.noreply.github.com>
2022-09-15 07:17:58 +0530

    Create app.sql

---
+++ Database/app.sql
@@ -0,0 +1,15 @@
+
+
+    
+CREATE DATABASE  IF NOT EXISTS `xtech`;
+USE `xtech`;
+
+CREATE USER 'xtech@'localhost' IDENTIFIED BY 'xtech';
+GRANT ALL PRIVILEGES ON *.* TO 'xtech'@'localhost' WITH GRANT OPTION;
+CREATE USER 'xtech'@'%' IDENTIFIED BY 'xtech';
+GRANT ALL PRIVILEGES ON *.* TO 'xtech'@'%' WITH GRANT OPTION;
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users`(id int primary key auto_increment,uname varchar(50),upwd varchar(50),uemail varchar(50),umobile var(10));
+
